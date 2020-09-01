fe1922427f9fbd79990fcfaf9bd810d71890fd35
swati98 <37444333+swati98@users.noreply.github.com>
2020-09-01 11:46:43 +0530

    Update printmanual.cpp

--- printmanual.cpp
+++ printmanual.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include "TeleColorCoder.h"
+#include <string>
+#include <fstream>
+#include <cstdio>
+using namespace std;
+using namespace TelCoColorCoder;
 
+extern int numberOfPairs;
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(constants::MajorColor major, constants::MinorColor minor);
+
+void WriteToFile()
+{
+	
+	fstream fout;
+	fout.open("Color-Number-Manual.txt", ios::app);
+	for(int i =1 ;i <=numberOfPairs;i++)
+	{
+		TelCoColorCoder::ColorPair color = GetColorFromPairNumber(i);
+		string color_string = color.ToString();
+		string output_string = to_string(i);
+		output_string += ": ";
+		output_string += color_string;
+		fout<<output_string;
+		fout<<" \n";
+	}
+	fout.close();
+
+}
